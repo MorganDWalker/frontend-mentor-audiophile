31a3d0c1e4bdb4f85b8b6c7fa48de161cfe77219
MorganDWalker <greenpakman@gmail.com>
2024-12-06 11:23:00 -0500

    Setting up homepage and adding assets

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,88 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import Cart from './assets/shared/desktop/icon-cart.svg';
+import Logo from './assets/shared/desktop/logo.svg';
+import HeadphoneImage from '';
+import SpeakerImage from '';
+import EarphonesImage from '';
+
+
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main>
+       <header className='block--background-black block__white-text'>
+        <nav>
+            <ul>
+              <li>
+              <Image src={Logo} alt='Audiophile logo' />
+              </li>
+              <li>Home</li>
+              <li>Headphones</li>
+              <li>Speakers</li>
+              <li>Earphones</li>
+              <li>
+                <a>
+                  <Image src={Cart} alt='Shopping Cart Icon'/>
+                </a>
+              </li>
+            </ul>
+          </nav>
+          
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          <p className='overline'>New product</p>
+          <h1>XX99 Mark II Headphones</h1>
+          Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.
+          See product
+        </header>
+        <section>
+          Headphones
+            Shop
+
+            Speakers
+            Shop
+
+            Earphones
+            Shop
+        </section>
+        
+        <section>
+          ZX9 speaker
+          Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.
+          See product
+
+          ZX7 speaker
+          See product
+
+          YX1 earphones
+          See product
+        </section>
+        
+        <article>
+          Bringing you the best audio gear
+          Located at the heart of New York City, Audiophile is the premier store for high end headphones, 
+          earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration 
+          rooms available for you to browse and experience a wide range of our products. Stop by our store 
+          to meet some of the fantastic people who make Audiophile the best place to buy your portable 
+          audio equipment.
+        </article>
+        
+        <footer>
+          <aside>
+            <nav>
+              Home
+              Headphones
+              Speakers
+              Earphones
+            </nav>
+          </aside>
+          Audiophile is an all in one stop to fulfill your audio needs. We&apos;re a small team of music lovers 
+          and sound specialists who are devoted to helping you get the most out of personal audio. Come and 
+          visit our demo facility - we&apos;re open 7 days a week.
+
+          Copyright 2021. All Rights Reserved
+        </footer>
+        
+    </main>
+   
   );
 }
